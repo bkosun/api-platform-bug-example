7586b10bac8bf8c73547117043d7550f70dec32a
Borislav Kosun <kosun.b@yandex.ua>
2021-02-01 10:25:20 +0200

    Make entity

---
+++ src/Entity/Foo.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\FooRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=FooRepository::class)
+ */
+class Foo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $foo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $bar;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFoo(): ?string
+    {
+        return $this->foo;
+    }
+
+    public function setFoo(string $foo): self
+    {
+        $this->foo = $foo;
+
+        return $this;
+    }
+
+    public function getBar(): ?string
+    {
+        return $this->bar;
+    }
+
+    public function setBar(string $bar): self
+    {
+        $this->bar = $bar;
+
+        return $this;
+    }
+}
